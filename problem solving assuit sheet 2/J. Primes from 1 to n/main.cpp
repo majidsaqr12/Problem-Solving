93d41e41aa16ce4d27439375b897fefb01539f0b
majidsaqr12 <smagedmohamed@gmail.com>
2023-06-21 17:52:26 +0300

    update

---
+++ problem solving assuit sheet 2/J. Primes from 1 to n/main.cpp
@@ -0,0 +1,30 @@
+// C++ program to display Prime numbers till N
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n)
+{
+	if (n == 1 || n == 0)
+		return false;
+
+	for (int i = 2; i <= n/2; i++) {
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+
+		if (isPrime(i))
+			cout << i << " ";
+	}
+
+	return 0;
+}
