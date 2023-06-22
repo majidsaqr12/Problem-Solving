2041af60cb5ac335ef8b0ccd52ff1111045493c1
majidsaqr12 <smagedmohamed@gmail.com>
2023-06-22 15:53:34 +0300

    update

--- problem solving assuit sheet 2/M. Lucky Numbers/main.cpp
+++ problem solving assuit sheet 2/M. Lucky Numbers/main.cpp
@@ -2,15 +2,48 @@
 
 using namespace std;
 
+bool isLucky(int num)
+{
+
+    bool is_lucky = false;
+
+    label: int z = num % 10;
+
+    if(num != 0)
+    {
+        if(z == 4 || z == 7)
+        {
+            is_lucky = true;
+            num = num / 10;
+            goto label;
+        }
+        else
+        {
+            is_lucky = false;
+        }
+    }
+    if(is_lucky) {return true;}
+    else{return false;}
+
+}
 int main()
 {
     int A, B;
 
     cin >> A >> B;
 
+    bool flag = false;
+
     for(int i = A; i <= B; i++)
     {
-
+        if(isLucky(i))
+        {
+            flag = true;
+            cout << i << ' ';
+        }
     }
+
+    if(flag == false) {cout << -1;}
+
     return 0;
 }
